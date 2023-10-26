5e7433ca728b13bfb3e57f5ffbd5b6a0249897fb
Hady2023 <138216010+Hady2023@users.noreply.github.com>
2023-10-26 11:21:08 +0300

    Delete ASS_2 directory

--- ASS_2/convert/confige.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#ifndef confige.h 
-#define confige.h 
-
-// MOOD of capital 
-#define Capital    large_later 
-
-#define Small        NOT 
-#endif 
--- ASS_2/convert/header.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#ifndef  header.h
-#define  header.h
-#define  small_later  1    //true statment for small 
-
-#define large_later   2   // true statment for large 
-#define NOT           0
-  // not statment for bothe 
-#endif
